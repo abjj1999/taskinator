8ceab283f487f9a444231d1620976d7526566a30
abjj1999 <abdullahalhilfi21@gmail.com>
2021-07-13 11:18:40 -0500

    some of 4.3.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,22 +96,4 @@ var createTaskActions = function(taskId){
 }
 
 
-var taskButtonHandler = function(event) {
-    console.log(event.target);
-
-    if (event.target.matches(".delete-btn")) {
-        console.log("you clicked a delete button!");
-        var taskId = event.target.getAttribute("data-task-id");
-        deleteTask(taskId);
-    }
-};
-var deleteTask = function(taskId){
-    console.log(taskId);
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
-    taskSelected.remove();
-};
 formEl.addEventListener("submit", taskFormHandler);
-
-pageContentEl.addEventListener("click", taskButtonHandler);
-
-
